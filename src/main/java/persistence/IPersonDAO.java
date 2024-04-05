7a1091a20f1a444a324a5f1c12dc2042e02ddb66
FranzMV <FranManzanedoValle@gmail.com>
2024-04-05 15:12:10 +0200

    add IPersonDAO.java

---
+++ src/main/java/persistence/IPersonDAO.java
@@ -0,0 +1,7 @@
+package persistence;
+
+import com.batch.entities.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IPersonDAO extends CrudRepository<Person, Long> {
+}
