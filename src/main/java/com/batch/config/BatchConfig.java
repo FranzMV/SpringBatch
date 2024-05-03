a3461cba1000858bec29f5e1ebd82ddf1b252d01
FranzMV <FranManzanedoValle@gmail.com>
2024-05-04 01:18:35 +0200

    rename BatchConfig.java

--- src/main/java/com/batch/config/BatchConfig.java
+++ src/main/java/com/batch/config/BatchConfig.java
@@ -5,19 +5,15 @@ import com.batch.steps.ItemProcessorStep;
 import com.batch.steps.ItemReaderStep;
 import com.batch.steps.ItemWriterStep;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableBatchProcessing
-public class BatchConfiguration {
+public class BatchConfig{
 
     @Autowired
     public JobBuilderFactory jobBuilderFactory;
