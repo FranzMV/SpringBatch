5ff9c92ab235139c6ef809677896afeca6470d5c
FranzMV <FranManzanedoValle@gmail.com>
2024-05-04 00:15:14 +0200

    Controller implementation

--- src/main/java/com/batch/constants/BatchConstants.java
+++ src/main/java/com/batch/constants/BatchConstants.java
@@ -2,7 +2,7 @@ package com.batch.constants;
 
 public class BatchConstants {
 
-      public static final String FILE_PATH_ORIGIN = "classpath:files/person.zip";
+      public static final String FILE_PATH_ORIGIN = "classpath:files/persons.zip";
       public static final String  FILE_PATH_DESTINATION = "classpath:files/destination/persons.csv";
       public static final char CSV_SEPARATOR =',';
       public static final String DATE_FORMAT ="dd/MM/yyyy HH:mm:ss";
